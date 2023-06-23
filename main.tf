031cab252b19a5f441d18bacfd7222b22615e69d
Amanda B <71290933+amandabasara@users.noreply.github.com>
2023-06-23 09:18:50 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+resource "aws_route53_zone" "primary" {
+  name = "example.com"
+}
